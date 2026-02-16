000451a264083916914ac2b0f300db612fe409ac
gabrieldaloiso <gabriel.daloiso@isen.yncrea.fr>
2026-02-16 15:43:12 +0100

    update

--- app/src/main/java/fr/isen/daloiso/thegreatestcocktailapp/DetailCocktailScreen.kt
+++ app/src/main/java/fr/isen/daloiso/thegreatestcocktailapp/DetailCocktailScreen.kt
@@ -2,6 +2,7 @@ package fr.isen.daloiso.thegreatestcocktailapp
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -9,19 +10,117 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import fr.isen.daloiso.thegreatestcocktailapp.ui.theme.TheGreatestCocktailAppTheme
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.FontWeight
+import fr.isen.daloiso.thegreatestcocktailapp.R
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.CardDefaults
+
+
 
 @Composable
 fun DetailCocktailScreen(name: String, modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .padding(16.dp)
+    ) {
         Image(
             painter = painterResource(id = R.drawable.cocktail),
-            contentDescription =  "Image du cocktail"
+            contentDescription =  "Cocktail picture",
+            modifier = Modifier
+                .fillMaxSize()
+                .height(200.dp)
         )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
         Text(
-            text = "Hello $name!"
+            text = "Moscow Mule",
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold
         )
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = "Click me")
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Text(
+            text = "Categorie: Alcohol",
+            fontSize = 14.sp,
+            color = MaterialTheme.colorScheme.secondary
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Text(
+            text = "Type of glass: Copper",
+            fontSize = 14.sp,
+            color = MaterialTheme.colorScheme.secondary
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Card(
+            modifier = Modifier.fillMaxWidth(),
+            elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+        ) {
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text(
+                    text = "Ingredients",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
+                )
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Text("• Vodka - 40 ml")
+                Text("• Lemon juice - 10 ml")
+                Text("• Sugar syrup - 10 ml")
+                Text("• Ginger Ale - 15 ml")
+                Text("• Mint leaves")
+
+            }
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Card(
+            modifier = Modifier.fillMaxWidth(),
+            elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+        ) {
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text(
+                    text = "Recipe",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
+                )
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Text(
+                    text = "Beat the heat with this crisp summer cooler. Shake the vodka, lemon juice, and syrup vigorously. Serve over ice, top with a splash of Ginger Ale, and finish with fresh mint leaves for a refreshing kick.",
+                    fontSize = 14.sp,
+                    lineHeight = 20.sp
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // Bouton (si nécessaire)
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Add to favorites")
         }
     }
 }
