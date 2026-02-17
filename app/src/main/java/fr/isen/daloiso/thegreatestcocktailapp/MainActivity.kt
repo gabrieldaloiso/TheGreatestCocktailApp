31f8f41cf632716a054d3a82b85a88aedefc299e
gabrieldaloiso <gabriel.daloiso@isen.yncrea.fr>
2026-02-17 11:56:29 +0100

    update

--- app/src/main/java/fr/isen/daloiso/thegreatestcocktailapp/MainActivity.kt
+++ app/src/main/java/fr/isen/daloiso/thegreatestcocktailapp/MainActivity.kt
@@ -12,7 +12,13 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Menu
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -21,11 +27,21 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
 import fr.isen.daloiso.thegreatestcocktailapp.screens.CategoriesScreen
+import fr.isen.daloiso.thegreatestcocktailapp.screens.BottomAppBar
 import fr.isen.daloiso.thegreatestcocktailapp.screens.DetailCocktailScreen
 import fr.isen.daloiso.thegreatestcocktailapp.ui.theme.TheGreatestCocktailAppTheme
+
+data class TabBarItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector
+)
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
@@ -34,6 +50,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             val context = LocalContext.current
+            val navController = rememberNavController()
+
+            val randomItem = TabBarItem(stringResource(id = R.string.tab_item_random), selectedIcon = Icons.Filled.Home, unselectedIcon = Icons.Outlined.Home)
+            val categoryItem = TabBarItem(stringResource(id = R.string.tab_item_category), selectedIcon = Icons.Filled.Menu, unselectedIcon = Icons.Outlined.Menu)
+            val favoriteItem = TabBarItem(stringResource(id = R.string.tab_item_favorite), selectedIcon = Icons.Filled.Favorite, unselectedIcon = Icons.Outlined.Favorite)
+
+            val tabItems = listOf(randomItem, categoryItem, favoriteItem)
+
             TheGreatestCocktailAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize(),
                     topBar = {
@@ -51,7 +75,8 @@ class MainActivity : ComponentActivity() {
                                 )
                             }
                         })
-                    }
+                    },
+                    bottomBar = { BottomAppBar(tabItems, navController) }
                 ) { innerPadding ->
 //                    Greeting(
 //                        name = "Android",
---
+++ app/src/main/java/fr/isen/daloiso/thegreatestcocktailapp/screens/BottomAppBar.kt
@@ -0,0 +1,54 @@
+package fr.isen.daloiso.thegreatestcocktailapp.screens
+
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.navigation.NavController
+import fr.isen.daloiso.thegreatestcocktailapp.TabBarItem
+
+@Composable
+fun BottomAppBar(
+    items: List<TabBarItem>,
+    navController: NavController
+) {
+    var selectedTabIndex by rememberSaveable { mutableStateOf(0) }
+
+    NavigationBar {
+        items.forEachIndexed { index, item ->
+            NavigationBarItem(
+                selected = selectedTabIndex == index,
+                onClick = {
+                    selectedTabIndex = index
+                    navController.navigate(item.title)
+                },
+                icon = {
+                    TabBarIcon(
+                        isSelected = selectedTabIndex == index,
+                        selectedIcon = item.selectedIcon,
+                        unselectedIcon = item.unselectedIcon,
+                        title = item.title
+                    )
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun TabBarIcon(
+    isSelected: Boolean,
+    selectedIcon: ImageVector,
+    unselectedIcon: ImageVector,
+    title: String
+) {
+    Icon(
+        imageVector = if (isSelected) selectedIcon else unselectedIcon,
+        contentDescription = title
+    )
+}
